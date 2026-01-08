503981d2dc32320b7654bbc28db716b1903b9f85
Neba Desmond <desmondneba134@gmail.com>
2026-01-08 01:15:35 +0100

    initial commit

---
+++ src/main/java/com/dezzy/springbootmodulithcourse/SpringBootModulithCourseApplication.java
@@ -0,0 +1,13 @@
+package com.dezzy.springbootmodulithcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootModulithCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootModulithCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dezzy/springbootmodulithcourse/SpringBootModulithCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dezzy.springbootmodulithcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootModulithCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
